9129ba10208b11a9c7aeb1d410d0078790fee01e
ragul <ragulraja893@gmail.com>
2026-09-03 22:30:48 +0530

    yml files trigger

--- pyss.py
+++ pyss.py
@@ -1,8 +1,4 @@
 print("First file is running")
-<<<<<<< HEAD
 print("First job completed successfully  ")
 
-print("done changes")
-=======
-print("First job completed successfully")
->>>>>>> 09cdab9c449cb8972b26c313d36567d66f30e29f
+print("done changes with secrets")
